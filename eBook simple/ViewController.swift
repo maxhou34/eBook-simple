9f6c42435a311686635a95ff3b73c930aea5943f
Max Hou <109076055+maxhou34@users.noreply.github.com>
2022-07-12 22:52:10 +0800

    Initial Commit

---
+++ eBook simple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eBook simple
+//
+//  Created by Chun on 2022/7/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
